77127cb074eaa2a17dd370acfa6647cb3b1b689e
HarrisonHenri <harrisonhenrisn@gmail.com>
2023-07-31 03:21:11 -0300

    fix: minor fixes

--- cypress/e2e/auth/auth.cy.ts
+++ cypress/e2e/auth/auth.cy.ts
@@ -1,17 +1,17 @@
 describe('Home', () => {
   beforeEach(function () {
-    // cy.fixture('user.json').then(user => {
-    //   this.user = user
-    // })
+    cy.fixture('user.json').then(user => {
+      this.user = user
+    })
 
     cy.visit('/')
   })
 
   it('should login successfully and go to home', function () {
-    // const { username, password } = this.user
+    const { username, password } = this.user
 
-    cy.get('[data-testid="username"]').type('bleveragei')
-    cy.get('[data-testid="password"]').type('UZGAiqPqWQHQ')
+    cy.get('[data-testid="username"]').type(username)
+    cy.get('[data-testid="password"]').type(password)
 
     cy.get('button').click()
 
