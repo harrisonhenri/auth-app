99c4450f752bcbdf4d5f0c0cf0c3fb0b3b4b8587
HarrisonHenri <harrisonhenrisn@gmail.com>
2023-07-31 22:32:37 -0300

    fix: minor fixes

--- src/__tests__/utils/http/is-token-expired/is-token-expired.spec.ts
+++ src/__tests__/utils/http/is-token-expired/is-token-expired.spec.ts
@@ -7,12 +7,15 @@ describe('isTokenExpired', () => {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsInVzZXJuYW1lIjoia21pbmNoZWxsZSIsImVtYWlsIjoia21pbmNoZWxsZUBxcS5jb20iLCJmaXJzdE5hbWUiOiJKZWFubmUiLCJsYXN0TmFtZSI6IkhhbHZvcnNvbiIsImdlbmRlciI6ImZlbWFsZSIsImltYWdlIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvYXV0cXVpYXV0LnBuZyIsImlhdCI6MTY5MDc0MDAxMCwiZXhwIjoxNjkwNzQzNjEwfQ._kc2G7LBC4c2Tz3UZRCv73tKeD-KtUmf0-hjTFNwo10'
     const token2 =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsInVzZXJuYW1lIjoia21pbmNoZWxsZSIsImVtYWlsIjoia21pbmNoZWxsZUBxcS5jb20iLCJmaXJzdE5hbWUiOiJKZWFubmUiLCJsYXN0TmFtZSI6IkhhbHZvcnNvbiIsImdlbmRlciI6ImZlbWFsZSIsImltYWdlIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvYXV0cXVpYXV0LnBuZyIsImlhdCI6MTY5MDc0MDM0NywiZXhwIjoxNjkwNzQzOTQ3fQ.Z8AuuDOMK9BOq8dYp0S6L2UMUikIGnexUOyqxjKyYHE'
+    const token3 = null
     const expected1 = true
     const expected2 = false
+    const expected3 = false
 
     return [
       { token: token1, expected: expected1 },
       { token: token2, expected: expected2 },
+      { token: token3, expected: expected3 },
     ]
   }
 
