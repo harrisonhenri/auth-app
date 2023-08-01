a811897591caa1a544a81efc3946aeb186226551
HarrisonHenri <harrisonhenrisn@gmail.com>
2023-07-31 22:42:41 -0300

    fix: minor fixes

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     video: false,
-    screenshotOnRunFailure: false,
+    screenshotOnRunFailure: true,
     baseUrl: 'http://localhost:5173',
   },
 })
